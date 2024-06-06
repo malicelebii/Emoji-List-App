ac9801f53c1bd6277c277bfe806478b430bb7d8d
malicelebii <mehmetalicelebii@gmail.com>
2024-06-06 13:36:26 +0300

    + Create SearchViewModelDelegate protocol

--- EmojiListApp/Modules/Search/SearchViewModel.swift
+++ EmojiListApp/Modules/Search/SearchViewModel.swift
@@ -1,7 +1,12 @@
 import Foundation
 
 protocol SearchViewModelDelegate {
+    var view: SearchViewDelegate? { get set }
     func getEmojisWith(name: String, completion: @escaping ([Emoji]) -> ())
+    func viewDidLoad()
+    func numberOfItemsInSection() -> Int
+    func cellForItem(at indexPath: IndexPath) -> Emoji
+    func didSelectItem(at: Int) -> Emoji
 }
 
 class SearchViewModel {
