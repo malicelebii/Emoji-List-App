b96560bf09db6f5955d5a1fcf5d007a92e75177a
malicelebii <mehmetalicelebii@gmail.com>
2024-06-06 14:16:16 +0300

    + Add view into FavoritesViewModelDelegate protocol as a property + Create view variable in VM to conform to protocol + Create init func for dependency injection

--- EmojiListApp/Modules/Favorites/FavoritesViewModel.swift
+++ EmojiListApp/Modules/Favorites/FavoritesViewModel.swift
@@ -1,12 +1,19 @@
 import Foundation
 
 protocol FavoritesViewModelDelegate {
+    var view: FavoritesViewDelegate? { get set }
     func addToFavorites(emoji: Emoji, completion: @escaping () -> ())
     func getAllFavoriteEmojis(completion: @escaping ([Emoji]) -> ())
     func deleteFavoriteWith(name: String, completion: @escaping () -> ())
 }
 
-class FavoritesViewModel {
+class FavoritesViewModel: FavoritesViewModelDelegate {
+    weak var view: FavoritesViewDelegate?
+    
+    init(view: FavoritesViewDelegate? = nil) {
+        self.view = view
+    }
+    
     func addToFavorites(emoji: Emoji, completion: @escaping () -> ()) {
         FavoriteEmojiCoreDataManager.shared.save(favoriteEmoji: emoji) {
             completion()
